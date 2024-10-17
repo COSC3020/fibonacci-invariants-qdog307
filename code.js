1a15f9e0e09cebccc448af3ffc4dea8b41347b70
Quincy Morris Reynolds <143751384+qdog307@users.noreply.github.com>
2024-10-17 09:54:09 -0600

    Fibcode

--- code.js
+++ code.js
@@ -1,3 +1,22 @@
 function fib(n) {
-    return n;
+    const fibArray = new Array(n + 1);
+
+    function computeFib(i) {
+        if (i === 0) {
+            fibArray[0] = 0;
+            return 0;
+        }
+        if (i === 1) {
+            fibArray[1] = 1;
+            return 1;
+        }
+        if (fibArray[i] === undefined) {
+            fibArray[i] = computeFib(i - 1) + computeFib(i - 2);
+        }
+        return fibArray[i];
+    }
+
+    computeFib(n);
+    return fibArray;
 }
+
